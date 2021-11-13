ae65c543c8a95a592e103435fa1b1f17b080e593
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2021-11-13 14:11:16 +0400

    Initial commit

---
+++ src/main/java/com/developia/booksforeverbody/BooksforeverbodyApplication.java
@@ -0,0 +1,13 @@
+package com.developia.booksforeverbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksforeverbodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksforeverbodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developia/booksforeverbody/BooksforeverbodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developia.booksforeverbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksforeverbodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
