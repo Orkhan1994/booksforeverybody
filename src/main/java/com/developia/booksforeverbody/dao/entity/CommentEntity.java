792dbdc86690d2880a69a2e9ab7f11c93e69e8aa
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2021-11-13 18:57:38 +0400

    Created CommentEntity

---
+++ src/main/java/com/developia/booksforeverbody/dao/entity/CommentEntity.java
@@ -0,0 +1,27 @@
+package com.developia.booksforeverbody.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "message")
+    private String message;
+
+}
