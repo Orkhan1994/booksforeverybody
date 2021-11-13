7c984ad31f48f9eaeda510823bfcffcd16434460
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2021-11-13 18:58:21 +0400

    Created OrderEntity

---
+++ src/main/java/com/developia/booksforeverbody/dao/entity/OrderEntity.java
@@ -0,0 +1,23 @@
+package com.developia.booksforeverbody.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "total_amount")
+    private Double totalAmount;
+}
