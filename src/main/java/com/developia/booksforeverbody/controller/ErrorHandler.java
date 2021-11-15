95f68d04e11289ce2969dab6434784139290b7af
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2021-11-15 14:59:11 +0400

    Created NotFoundException and ErrorHandler

---
+++ src/main/java/com/developia/booksforeverbody/controller/ErrorHandler.java
@@ -0,0 +1,26 @@
+package com.developia.booksforeverbody.controller;
+
+import com.developia.booksforeverbody.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handler(HttpServletRequest request,NotFoundException ex){
+        ModelAndView mv=new ModelAndView();
+
+        mv.setViewName("error-page");
+        mv.addObject("details",ex.getMessage());
+        mv.addObject("path",request.getRequestURL());
+
+        return mv;
+    }
+}
